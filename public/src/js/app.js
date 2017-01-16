9e3465dbe274dbae322954c5b82df0ca0d09bed9
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-01-16 15:55:03 +1000

    Create side panels

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,16 @@
 // init application
 $(function() {
   // create map
-  var map = new ol.Map({});
+  var map = new ol.Map({
+    target: "map",
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: [0, 0],
+      zoom: 2
+    })
+  });
 });
