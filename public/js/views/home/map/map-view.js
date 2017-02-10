ebe1f7f5e91f5922a2096c8e09c88f112cf985bb
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-02-10 15:20:00 +1000

    Add smooth transitions on layer display

--- public/js/views/home/map/map-view.js
+++ public/js/views/home/map/map-view.js
@@ -30,7 +30,60 @@ define([
         console.log("layer.cid = " + layer.get("cid"));
         if (layer.get("cid") === model.cid) {
           console.log("change visible to " + value);
-          layer.set("visible", value);
+          /*
+          if (value) {
+            // show layer
+            var from = 0;
+            var to = 1;
+            console.log("from: " + from + ", to: " + to);
+            console.log("layer.visible: " + layer.getVisible());
+            console.log("layer.opacity: " + layer.getOpacity());
+            $({n: from}).animate({n: to}, {
+              duration: 500,
+              start: function() {
+                layer.setVisible(true);
+              },
+              step: function(now, fx) {
+                layer.setOpacity(now);
+              }
+            });
+          } else {
+            // hide layer
+            var from = layer.getOpacity();
+            var to = 0;
+            console.log("from: " + from + ", to: " + to);
+            console.log("layer.visible: " + layer.getVisible());
+            console.log("layer.opacity: " + layer.getOpacity());
+            $({n: from}).animate({n: to}, {
+              duration: 500,
+              step: function(now, fx) {
+                layer.setOpacity(now);
+              },
+              complete: function() {
+                layer.setVisible(false);
+              }
+            });
+          }
+          */
+          /*
+          var from = value ? 0 : layer.getOpacity();
+          var to = value ? 100 : 0;
+          $({opacity: from}).animate({opacity: to}, {
+            duration: 1000,
+            start: function() {
+              console.log("start easing");
+            },
+            step: function(now, fx) {
+              layer.setOpacity(now);
+            },
+            complete: function() {
+              console.log("complete easing");
+            }
+          });
+          */
+          /*
+          layer.setVisible(value);
+          */
         }
       })
     }
