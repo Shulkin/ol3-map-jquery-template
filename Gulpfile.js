6ceed04ca4a4c8dcd4c222c6066d95b79cbf5a66
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-02-27 15:30:57 +1000

    Fixed gulp build

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require("gulp");
+var rimraf = require("rimraf");
+var clean = require("gulp-rimraf");
 var concatCss = require("gulp-concat-css");
 var minifyCss = require("gulp-minify-css");
 var rename = require("gulp-rename");
@@ -8,15 +10,34 @@ var connect = require("gulp-connect");
 var uglify = require("gulp-uglify");
 var imagemin = require("gulp-imagemin");
 var pngquant = require("imagemin-pngquant");
-// clean build folder
-/*
-gulp.task("clean", function() {
-  gulp.src("")
+// delete build folder
+gulp.task("delete:build", function(done) {
+  return rimraf("./build", done);
 });
-*/
-// css
-gulp.task("css", function() {
-  gulp.src("./public/css/*.css")
+// clean separate folders
+gulp.task("clean:js", function() {
+  return gulp.src("./build/js/*", {read: false}).pipe(clean());
+});
+gulp.task("clean:css", function() {
+  return gulp.src("./build/css/*", {read: false}).pipe(clean());
+});
+gulp.task("clean:public", function() {
+  // delete index.html and favicon.ico
+  return gulp.src("./build/*{html,ico}", {read: false}).pipe(clean());
+});
+gulp.task("clean:templates", function() {
+  return gulp.src("./build/templates/*", {read: false}).pipe(clean());
+});
+// uglify javascript files
+gulp.task("js", ["clean:js"], function() {
+  return gulp.src("./public/js/**/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js/"))
+    .pipe(connect.reload());
+});
+// concatenate styles
+gulp.task("css", ["clean:css"], function() {
+  return gulp.src("./public/css/*.css")
     .pipe(concatCss("style.min.css"))
     .pipe(minifyCss({compatibility: 'ie8'}))
     .pipe(autoprefixer({
@@ -26,35 +47,21 @@ gulp.task("css", function() {
     .pipe(gulp.dest("./build/css/"))
     .pipe(connect.reload());
 });
-// copy index.html to build folder
-gulp.task("copy:html", function() {
-  gulp.src("./public/*.html")
+// copy public files
+gulp.task("public", ["clean:public"], function() {
+  return gulp.src("./public/*{html,ico}")
     .pipe(gulp.dest("./build/"))
     .pipe(connect.reload());
 });
-// copy favicon.ico to build folder
-gulp.task("copy:favicon", function() {
-  gulp.src("./public/*.ico")
-    .pipe(gulp.dest("./build/"))
-    .pipe(connect.reload());
-})
-// html templates
-gulp.task("templates", function() {
-  // copy html templates
-  gulp.src("./public/templates/**/*.html")
+// copy html templates
+gulp.task("templates", ["clean:templates"], function() {
+  return gulp.src("./public/templates/**/*.html")
     .pipe(gulp.dest("./build/templates/"))
-    .pipe(connect.reload());
-});
-// minify javascript files
-gulp.task("js", function() {
-  gulp.src("./public/js/**/*.js")
-    .pipe(uglify())
-    .pipe(gulp.dest("./build/js/"))
     .pipe(connect.reload());
 });
 // copy images to build folder
 gulp.task("img", function() {
-  gulp.src("./public/imgs/*.png")
+  return gulp.src("./public/imgs/*.png")
     .pipe(imagemin({
         progressive: true,
         svgoPlugins: [{removeViewBox: false}],
@@ -63,7 +70,7 @@ gulp.task("img", function() {
     .pipe(gulp.dest("./build/imgs/"))
     .pipe(connect.reload());
 });
-// connect
+// connect to server
 gulp.task("connect", function() {
   connect.server({
     root: "build",
@@ -72,17 +79,19 @@ gulp.task("connect", function() {
 });
 // build source files on change
 gulp.task("watch", function() {
-  gulp.watch("./public/css/*.css", ["css"]);
-  gulp.watch("./public/*.html", ["html"]);
-  gulp.watch("./public/templates/**/.html", ["templates"])
   gulp.watch("./public/js/**/*.js", ["js"]);
+  gulp.watch("./public/css/*.css", ["css"]);
+  gulp.watch("./public/*.html", ["public"]);
+  gulp.watch("./public/templates/**/.html", ["templates"])
 });
 // default gulp task
 gulp.task("default", [
-  // build source files
-  "css", "js", "img"
-  // copy html templates
-  "copy:html", "copy:favicon", "templates",
-  // connect to server and watch
-  "connect", "watch"
-]);
+  "delete:build", // delete previous build
+], function() {
+  gulp.start(
+    // build source and copy public files
+    "js", "css", "public", "templates", "img",
+    // connect to server and watch
+    "connect", "watch"
+  );
+});
